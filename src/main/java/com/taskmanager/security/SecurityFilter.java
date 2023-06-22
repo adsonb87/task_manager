22adf849a9c44606e52331c72563ae2664230275
adsonb87 <adsonb87@gmail.com>
2023-06-22 20:27:44 -0300

    Criação da classe Security Filter

--- src/main/java/com/taskmanager/security/SecurityFilter.java
+++ src/main/java/com/taskmanager/security/SecurityFilter.java
@@ -6,10 +6,15 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Objects;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
@@ -20,8 +25,33 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
     private String usuarioRepository;
 
+    @Autowired
+    private UserDetails usuario;
+
+    private String _getTokenFromRequest(HttpServletRequest request){
+        String authHeader = request.getHeader("Authorization");
+
+        if(Objects.nonNull(authHeader)){
+            return authHeader.replace("Bearer", "");
+        }
+        return null;
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        
+        String token = _getTokenFromRequest(request);
+
+        if(Objects.nonNull(token)){
+            String subject = tokenService.getSubject(token);
+
+            //Quando criar o repository do usuario implementar
+            //UserDetails usuario = usuarioRepository.findByEmail(subject);
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(usuario,null, null);
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request,response);
     }
 }
