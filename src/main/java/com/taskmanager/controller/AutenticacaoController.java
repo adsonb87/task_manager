28925355ceeed13806103aa61d062a7d0c2b94b3
adsonb87 <adsonb87@gmail.com>
2023-06-22 20:47:08 -0300

    Criação autenticacao controller

--- src/main/java/com/taskmanager/controller/AutenticacaoController.java
+++ src/main/java/com/taskmanager/controller/AutenticacaoController.java
@@ -1,4 +1,30 @@
 package com.taskmanager.controller;
 
+import com.taskmanager.service.AuthenticationService;
+import com.taskmanager.service.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/auth", produces = "application/json")
 public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @Autowired
+    private TokenService tokenService;
+
+    //Usuario repository
+
+
+
 }
