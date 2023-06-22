d6f0d17cc8a209fa913a0f169bf8de947b747a55
adsonb87 <adsonb87@gmail.com>
2023-06-22 20:18:40 -0300

    Criação da classe token service e Security Configuration

---
+++ src/main/java/com/taskmanager/service/TokenService.java
@@ -0,0 +1,62 @@
+package com.taskmanager.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret.key}")
+    private String SECRET_KEY;
+
+    @Value("${api.security.token.issuer}")
+    private String TOKEN_ISSUER;
+
+    //Variável temporária que substitui o login passado
+    //por parametro na funcao
+    private String loginUsuario;
+    private Long idUsuario;
+
+    private Instant _expirationDate() {
+        // O token expira em 30 dias de acordo com o fuso horário do Brasil
+        return LocalDateTime.now().plusDays(30).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+    public String generateToken(){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
+
+            return JWT.create()
+                    .withIssuer(TOKEN_ISSUER)
+                    .withSubject(loginUsuario)
+                    .withExpiresAt(_expirationDate())
+                    .withClaim("id", idUsuario)
+                    .sign(algorithm);
+        }catch (JWTCreationException exception){
+            throw new RuntimeException("Erro ao gerar o token JWT.", exception);
+        }
+    }
+
+    public String getSubject(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
+
+            return JWT.require(algorithm)
+                    .withIssuer(TOKEN_ISSUER)
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch (JWTVerificationException exception){
+            throw new RuntimeException("Token JWT inválido ou expirado.", exception);
+        }
+    }
+
+}
