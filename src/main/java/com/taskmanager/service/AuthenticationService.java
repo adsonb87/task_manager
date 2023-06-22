33246fe2e07f02bb332e15eef693ba8b88fea764
adsonb87 <adsonb87@gmail.com>
2023-06-22 20:31:52 -0300

    Criação do Authentication Service

--- src/main/java/com/taskmanager/service/AuthenticationService.java
+++ src/main/java/com/taskmanager/service/AuthenticationService.java
@@ -1,4 +1,23 @@
 package com.taskmanager.service;
 
-public class AuthenticationService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+
+    //Realizar a criacao do usuário repository para retornar
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //Retornar o e-mail do usuario que será utilizado como login
+        return null;
+    }
+
+    public BCryptPasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
